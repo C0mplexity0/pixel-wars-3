4603f16fd5c3cfb066bc94441a15848e8cdb4dc0
C0mplexity0 <106925362+C0mplexity0@users.noreply.github.com>
2025-05-11 12:28:46 +0100

    feat: New colours

--- packages/pixel-wars-core/player/index.ts
+++ packages/pixel-wars-core/player/index.ts
@@ -27,7 +27,7 @@ export default class Player {
 
     this.onWorldChangeEvent = new PixelWarsEvent()
 
-    this.colourInventory = [1, 2]
+    this.colourInventory = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
     this.selectedColour = 0
 
     this.onColourInventoryUpdatedEvent = new PixelWarsEvent()
