93fbf8079470673e3fb39eaf03fe411928b1793c
C0mplexity0 <106925362+C0mplexity0@users.noreply.github.com>
2025-05-12 19:11:38 +0100

    refactor: Remove completely unnecessary useState

--- packages/pixel-wars-client/src/components/ui/Button.tsx
+++ packages/pixel-wars-client/src/components/ui/Button.tsx
@@ -1,24 +1,10 @@
-import { useState } from "react";
 import { twMerge } from "tailwind-merge";
 
 export default function Button({ className, ...props }: React.ButtonHTMLAttributes<HTMLButtonElement>) {
-  const [hovered, setHovered] = useState(false)
-  
-    const onHover = () => {
-      setHovered(true)
-    }
-  
-    const onHoverLoss = () => {
-      setHovered(false)
-    }
-  
+
   return (
     <button 
-      className={twMerge(`border-2 p-1 cursor-pointer ${hovered ? "bg-button-hovered" : "bg-white"}`, className)} 
-      onMouseEnter={onHover}
-      onMouseLeave={onHoverLoss}
-      onFocus={onHover}
-      onBlur={onHoverLoss}
+      className={twMerge("border-2 p-1 cursor-pointer bg-white hover:bg-button-hovered", className)} 
       {...props} 
     />
   )
